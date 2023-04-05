dcf376e554f3e8c78f97d9b71523aae7aebe5484
Athar Noor Mohammad Rafee <64426953+Inmoresentum@users.noreply.github.com>
2023-04-05 22:00:06 +0600

    Refactored the code.

--- src/project.v
+++ src/project.v
@@ -1,64 +1,70 @@
-module project(clk, A, B, opcode, C, car, sign, zero);
-input clk;
-input [3:0] A;
-input [3:0] B;
-input [2:0] opcode;
-output reg [3:0] C;
-reg [2:0] state = 0;
-output reg car = 0, sign, zero;
-
-always @(posedge clk) begin
+module project(input clk, input [3:0] A, input [3:0] B,
+ input [2:0] opcode, output reg [3:0] C,
+  output reg carr, output reg sign, output reg zero);
+// Will be using state to indicate state.
+reg [1:0] state;
+always @ (posedge clk) begin
     case (state)
-        0: begin // Idle state
-               C <= C; // Output C retains the result of last operation performed
-               if (opcode == 3'b011) begin
-                   state <= 1; // Move to next state for NAND operation
-                   C[0] <= ~(A[0] & B[0]); // Perform NAND operation on LSBs and update C
-               end
-               else begin
-                   state <= 0; // Stay in idle state
-               end
-           end
-        1: begin // State 1
-               C[1] <= ~(A[1] & B[1]); // Perform NAND operation on next bit and update C
-               if (opcode != 3'b011) begin
-                   state <= 0; // Return to idle state if opcode is not 010 NAND
-               end
-               else if (state == 1 && opcode == 3'b011 && A == 4'b0000 && B == 4'b0000) begin
-                   state <= 0; // Return to idle state if A and B are both 0000
-               end
-               else begin
-                   state <= 2; // Move to next state
-               end
-           end
-        2: begin // State 2
-               C[2] <= ~(A[2] & B[2]); // Perform NAND operation on next bit and update C
-               if (opcode != 3'b011) begin
-                   state <= 0; // Return to idle state if opcode is not 010 NAND
-               end
-               else if (state == 2 && opcode == 3'b011 && A == 4'b0000 && B == 4'b0000) begin
-                   state <= 0; // Return to idle state if A and B are both 0000
-               end
-               else begin
-                   state <= 3; // Move to next state
-               end
-           end
-        3: begin // State 3
-               C[3] <= ~(A[3] & B[3]); // Perform NAND operation on MSBs and update C
-               sign <= C[3];
-			   zero <= (C == 4'b0000);
-               if (opcode != 3'b011) begin
-                   state <= 0; // Return to idle state if opcode is not 010 NAND
-               end
-               else begin
-                   state <= 0; // Return to idle state after operation is complete
-               end
-           end
-        default: begin
-                     state <= 0; // Return to idle state if undefined state is reached
-                 end
+        2'b00: begin
+            case (opcode)
+                3'b000: begin
+                    C <= 4'b0000; //RESET operation
+                    carr <= 1'b0;
+                    sign <= 1'b0;
+                    zero <= 1'b1;
+                end
+                3'b001: begin //XOR operation on LSBs
+                    C[0] <= A[0] ^ B[0];
+                    zero <= C[0] == 1'b0;
+                end
+                3'b011: begin //NAND operation on LSBs
+                    C[0] <= ~(A[0] & B[0]);
+                    zero <= C[0] == 1'b0;
+                end
+            endcase
+            state <= 2'b01;
+        end
+        2'b01: begin
+            case (opcode)
+                3'b001: begin //XOR operation on next bit
+                    C[1] <= A[1] ^ B[1];
+                    zero <= zero & (C[1] == 1'b0);
+                end
+                3'b011: begin //NAND operation on next bit
+                    C[1] <= ~(A[1] & B[1]);
+                    zero <= zero & (C[1] == 1'b0);
+                end
+            endcase
+            state <= 2'b10;
+        end
+        2'b10: begin
+            case (opcode)
+                3'b001: begin //XOR operation on next bit
+                    C[2] <= A[2] ^ B[2];
+                    zero <= zero & (C[2] == 1'b0);
+                end
+                3'b011: begin //NAND operation on next bit
+                    C[2] <= ~(A[2] & B[2]);
+                    zero <= zero & (C[2] == 1'b0);
+                end
+            endcase
+            state <= 2'b11;
+        end
+        2'b11: begin
+            case (opcode)
+                3'b001: begin //XOR operation on MSBs
+                    C[3] <= A[3] ^ B[3];
+                    sign <= C[3];
+                    zero <= C == 4'b0000;
+                end
+                3'b011: begin //NAND operation on MSBs
+                                    C[3] <= ~(A[3] & B[3]);
+                    sign <= C[3];
+                    zero <= C == 4'b0000;
+                end
+            endcase
+            state <= 2'b00;
+        end
     endcase
-  
 end
-
-endmodule
+endmodule 
