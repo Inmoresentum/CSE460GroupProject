fbd7a0563d2530e060a72eaa4e98385887264d9e
Athar Noor Mohammad Rafee <64426953+Inmoresentum@users.noreply.github.com>
2023-04-12 05:23:14 +0600

    Added rest of the instructions -Inmoresentum
    Added `ADD` and `SUB` operations. Furthermore, made sure that `SUB` operation is signed just as question wanted.

--- src/project.v
+++ src/project.v
@@ -1,8 +1,8 @@
 module project(input clk, input [3:0] A, input [3:0] B,
  input [2:0] opcode, output reg [3:0] C,
   output reg carr, output reg sign, output reg zero);
-// Will be using state to indicate state.
-reg [1:0] state;
+// Will be using state to indicate state of the machine.
+reg [1:0] state = 0;
 always @ (posedge clk) begin
     case (state)
         2'b00: begin
@@ -13,55 +13,99 @@ always @ (posedge clk) begin
                     sign <= 1'b0;
                     zero <= 1'b1;
                 end
-                3'b001: begin //XOR operation on LSBs
-                    C[0] <= A[0] ^ B[0];
+                3'b001: begin //XNOR operation on LSBs
+                    C[0] <= ~(A[0] ^ B[0]);
                     zero <= C[0] == 1'b0;
                 end
+                3'b010: begin //SUB operation on LSBs
+                    {carr, C[0]} <= B[0] - A[0];
+                    zero <= C[0] == 1'b0;
+               end
                 3'b011: begin //NAND operation on LSBs
                     C[0] <= ~(A[0] & B[0]);
                     zero <= C[0] == 1'b0;
                 end
+                3'b100: begin //ADD operation on LSBs
+                    {carr, C[0]} <= A[0] + B[0];
+                    zero <= C[0] == 1'b0;
+                end
+                
+                
             endcase
             state <= 2'b01;
         end
         2'b01: begin
             case (opcode)
-                3'b001: begin //XOR operation on next bit
-                    C[1] <= A[1] ^ B[1];
+                3'b001: begin //XNOR operation on next bit
+                    C[1] <= ~(A[1] ^ B[1]);
                     zero <= zero & (C[1] == 1'b0);
                 end
+                3'b010: begin //SUB operation on next bit
+                    {carr, C[1]} <= B[1] - A[1] - carr;
+                    zero <= zero & (C[1] == 1'b0);
+                end 
                 3'b011: begin //NAND operation on next bit
                     C[1] <= ~(A[1] & B[1]);
                     zero <= zero & (C[1] == 1'b0);
                 end
+                3'b100: begin //ADD operation on next bit
+                    {carr, C[1]} <= A[1] + B[1] + carr;
+                    zero <= zero & (C[1] == 1'b0);
+                end  
+                 
+                    
             endcase
             state <= 2'b10;
         end
         2'b10: begin
             case (opcode)
-                3'b001: begin //XOR operation on next bit
-                    C[2] <= A[2] ^ B[2];
+                3'b001: begin //XNOR operation on next bit
+                    C[2] <= ~(A[2] ^ B[2]);
+                    zero <= zero & (C[2] == 1'b0);
+                end
+                3'b010: begin //SUB operation on next bit
+                    {carr, C[2]} <= B[2] - A[2] - carr;
                     zero <= zero & (C[2] == 1'b0);
                 end
                 3'b011: begin //NAND operation on next bit
                     C[2] <= ~(A[2] & B[2]);
                     zero <= zero & (C[2] == 1'b0);
                 end
+                3'b100: begin //ADD operation on next bit
+                    {carr, C[2]} <= A[2] + B[2] + carr;
+                    zero <= zero & (C[2] == 1'b0);
+                end  
+                  
             endcase
             state <= 2'b11;
         end
         2'b11: begin
             case (opcode)
-                3'b001: begin //XOR operation on MSBs
-                    C[3] <= A[3] ^ B[3];
+                3'b001: begin //XNOR operation on MSBs
+                    C[3] <= ~(A[3] ^ B[3]);
                     sign <= C[3];
                     zero <= C == 4'b0000;
+                end
+                3'b010: begin //SUB operation on MSBs
+                 {carr, C[3]} <= B[3] - A[3] - carr;
+                 sign <= C[3];
+                 zero <= C == 4'b0000;
+                   if (B[3] < A[3]) begin //if result is negative, take two's complement of result
+                       C <= ~C + 4'b0001;
+                       sign <= C[3];
+                   end
                 end
                 3'b011: begin //NAND operation on MSBs
                                     C[3] <= ~(A[3] & B[3]);
                     sign <= C[3];
                     zero <= C == 4'b0000;
                 end
+                3'b100: begin //ADD operation on MSBs
+                    {carr, C[3]} <= A[3] + B[3] + carr;
+                    sign <= C[3];
+                    zero <= C == 4'b0000;
+                end
+                
             endcase
             state <= 2'b00;
         end
